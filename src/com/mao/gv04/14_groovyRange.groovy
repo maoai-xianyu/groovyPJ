b7f6bb157f0cd05782aec0c2d486c7576eafc619
zhangkun <zhangkun35@maoyan.com>
2021-09-28 11:16:00 +0800

    groovy 数据结构学习 range 继承与 list  public interface Range<T extends Comparable> extends List<T>

---
+++ src/com/mao/gv04/14_groovyRange.groovy
@@ -0,0 +1,48 @@
+package com.mao.gv04
+
+import org.w3c.dom.ranges.Range
+
+/**
+ * 范围的定义
+ */
+
+def range = 1..10
+println range[0]
+println range.contains(10)
+println range.from // 起始
+println range.to // 终止
+
+// 遍历
+range.each {
+    println it
+}
+for(i in range){
+    println i
+}
+
+groovy.lang.Range
+def result = getGrade(90)
+println result
+//
+def getGrade(Number number){
+    def result
+    switch (number){
+        case 0..<60:
+            result = '不及格'
+            break
+        case 60..<70:
+            result = '及格'
+            break
+        case 70..<80:
+            result = '中等'
+            break
+        case 80..<90:
+            result = '良好'
+            break
+        default:
+            result = '优秀'
+            break
+    }
+    return result
+}
+
